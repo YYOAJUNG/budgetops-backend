ad102db57691baebf49f3163da4bc0af91bbb38f
nAhYUN22 <nahyunee@192.168.nate.com>
2025-10-06 09:57:01 +0900

    fix: deploy.yml

---
+++ src/main/java/com/budgetops/backend/config/WebCorsConfig.java
@@ -0,0 +1,21 @@
+package com.budgetops.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Configuration
+public class WebCorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://budgetops.work")
+                        .allowedMethods("GET","POST","PUT","PATCH","DELETE","OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
