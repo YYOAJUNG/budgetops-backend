546b5b4540c497ffad1e124dcd48077843208006
nAhYUN22 <lily.nahyunlee@gmail.com>
2025-10-16 16:47:14 +0900

    fix: deploy.yml

--- src/main/java/com/budgetops/backend/config/WebCorsConfig.java
+++ src/main/java/com/budgetops/backend/config/WebCorsConfig.java
@@ -1,7 +1,10 @@
 package com.budgetops.backend;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebCorsConfig {
