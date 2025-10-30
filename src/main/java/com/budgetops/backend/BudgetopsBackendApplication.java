c242339c343e9c44936947e82ea76e9466d29761
yejinj <sitting077@gmail.com>
2025-10-30 17:33:58 +0900

    chore(config): 패키지 스캔 수정 및 민감키 기본값 제거
    관련: #9

--- src/main/java/com/budgetops/backend/BudgetopsBackendApplication.java
+++ src/main/java/com/budgetops/backend/BudgetopsBackendApplication.java
@@ -1,9 +1,9 @@
-package com.example.budgetops_backend;
+package com.budgetops.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.budgetops.backend"})
 public class BudgetopsBackendApplication {
 
 	public static void main(String[] args) {
