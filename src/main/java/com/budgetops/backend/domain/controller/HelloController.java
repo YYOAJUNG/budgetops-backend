eecf6dd47d87be00705a6bdcc818a339541594a1
nAhYUN22 <lily.nahyunlee@gmail.com>
2025-11-13 00:54:07 +0900

    feat: Azure 리소스 조회 기능

--- src/main/java/com/budgetops/backend/domain/controller/HelloController.java
+++ src/main/java/com/budgetops/backend/domain/controller/HelloController.java
@@ -1,5 +1,4 @@
-
-package com.budgetops.backend;
+package com.budgetops.backend.domain.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
